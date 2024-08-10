bb8e8fffcd93b0697a1af773ee1164ec09afc7a1
ShabirPolaris <shabir.ahmad@furnishedfinder.com>
2024-08-10 12:49:43 +0500

    updated name and email validation

--- CleanAuth.CoreBusiness/ValueObjects/Email.cs
+++ CleanAuth.CoreBusiness/ValueObjects/Email.cs
@@ -1,19 +1,38 @@
-﻿namespace CleanAuth.CoreBusiness.ValueObjects
+﻿using CleanAuth.CoreBusiness.Exceptions;
+using CleanAuth.CoreBusiness.RegexUtilities;
+
+namespace CleanAuth.CoreBusiness.ValueObjects
 {
-    public class Email : EmailBase
+    public class Email
     {
+        private string _value;
         public Email(string value, string propertyName)
-            : base(value)
         {
-            Validate(value, propertyName);
+            if (value != null)
+                _value = value.ToLower();
+            else
+                _value = value;
+
+            Validate(_value, propertyName);
         }
 
-        public static new void Validate(string value, string propertyName)
+        public static void Validate(string value, string propertyName)
         {
             if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentNullException($"{propertyName} is required.");
+                throw new InvalidEmailException($"{propertyName} is required.");
+         
+            RegexEmailValidator validator = new RegexEmailValidator();
 
-            EmailBase.Validate(value, propertyName);
+            if (!validator.IsValidEmail(value))
+                throw new InvalidEmailException($"{propertyName} is not valid.");
+
+            if (value.Length > 120)
+                throw new InvalidEmailException($"{propertyName} can be up to 120 chars long.");
         }
+
+        public override string ToString() => _value;
+
+        public static implicit operator string(Email email) => email._value;
     }
+
 }
--- CleanAuth.CoreBusiness/ValueObjects/Name.cs
+++ CleanAuth.CoreBusiness/ValueObjects/Name.cs
@@ -1,16 +1,34 @@
-﻿namespace CleanAuth.CoreBusiness.ValueObjects
+﻿using CleanAuth.CoreBusiness.Exceptions;
+
+namespace CleanAuth.CoreBusiness.ValueObjects
 {
-    public class Name : TextBase
+    public class Name
     {
         private const int _maxAllowedLength = 100;
-
-        public Name(string value, int maxLength, string propertyName = "Name", bool isRequired = true)
-            : base(value, maxLength, propertyName, _maxAllowedLength, isRequired)
-        { }
-
-        public static void Validate(string value, int maxLength, bool isRequired)
+        string _value;
+        public Name(string value, int maxLength, string propertyName = "Name", bool isRequired = true)    
         {
-            Validate(value, maxLength, "Name", _maxAllowedLength, isRequired);
+            Validate(value, maxLength, propertyName, _maxAllowedLength, isRequired);
+            _value = value;
         }
+
+        public static void Validate(string value, int maxLength, string propertyName, int allowedMaxLength, bool isRequired)
+        {
+            if (isRequired && string.IsNullOrWhiteSpace(value))
+                throw new InvariantException($"{propertyName} cannot be empty.");
+
+            if (isRequired && maxLength <= 0)
+                throw new InvariantException($"Max length must be more than zero for a required {propertyName}.");
+
+            if (maxLength > allowedMaxLength)
+                throw new InvariantException($"{propertyName} cannot be more than {allowedMaxLength} characters.");
+
+            if (value != null && value.Length > maxLength)
+                throw new InvariantException($"{propertyName} cannot be more than {maxLength} characters.");
+        }
+
+        public override string ToString() => _value;
+
+        public static implicit operator string(Name name) => name._value;
     }
 }
